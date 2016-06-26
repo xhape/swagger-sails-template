8757f6f1e74d94f785c6c84344b2fa8e22259329
Angelo Javonitalla <angelo.javonitalla@gmail.com>
2016-06-26 18:12:38 +0800

    Integrated swagger with a few compromise (i.e. blueprint issue)

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # swagger-sails-template
 
 A Swagger [Sails](http://sailsjs.org) application.
+
+### Swagger
+
+To edit swagger API specification run the following on the command prompt
+
+swagger project edit
---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    guid: {
+      type: "string",
+      "uuid" : true,
+      "primaryKey": true,
+      "unique": true
+    },
+
+    userId: {
+      type: "string"
+    }
+  }
+};
+
