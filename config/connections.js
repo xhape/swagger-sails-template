3579f85faa28756553944afa57f361935fb80e48
Angelo Javonitalla <angelo.javonitalla@gmail.com>
2016-06-26 12:24:58 +0800

    Adding app.json Production config Sorting package.json

--- config/connections.js
+++ config/connections.js
@@ -65,6 +65,11 @@ module.exports.connections = {
   //   database: 'your_mongo_db_name_here' //optional
   // },
 
+  mongolab: {
+    adapter: 'sails-mongo',
+    url: process.env.MONGODB_URI
+  }
+
   /***************************************************************************
   *                                                                          *
   * PostgreSQL is another officially supported relational database.          *
--- config/models.js
+++ config/models.js
@@ -17,7 +17,7 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  // connection: 'localDiskDb',
+  connection: 'localDiskDb',
 
   /***************************************************************************
   *                                                                          *
@@ -27,6 +27,13 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  // migrate: 'alter'
+  migrate: 'alter',
 
+  /***************************************************************************************************************
+   * A flag to toggle schemaless or schema mode in databases that support schemaless data structures.            *
+   * If turned off, this will allow you to store arbitrary data in a record.                                     *
+   * If turned on, only attributes defined in the model's attributes object will be stored.                      *
+   * For adapters that don't require a schema, such as Mongo or Redis, the default setting is schema:false       *
+   ***************************************************************************************************************/
+  schema: true
 };
